5126ed5ba097525cb6797be3dc1bd95c731a3a6d
pikesley <sam.pikesley@demandlogic.co.uk>
2025-01-24 14:40:57 +0000

    Refine asset-path finding

--- lib/asset_path.py
+++ lib/asset_path.py
@@ -3,9 +3,10 @@ import os
 
 apps = os.listdir("/apps")
 path = ""
+ASSET_PATH = "apps/clock/"
+
 for a in apps:
     if a == "pikesley_tildagon_clock":
+        print("found published-app-dir")
         path = "/apps/" + a
         ASSET_PATH = path + "/assets/"
-    else:
-        ASSET_PATH = "apps/clock/"
