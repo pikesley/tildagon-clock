196d37b7963745a9de91b93eaf759bd173880b6e
Sam Pikesley <sam.pikesley@demandlogic.co.uk>
2026-04-07 21:59:40 +0100

    Fix asset-path

--- common/asset_path.py
+++ common/asset_path.py
@@ -10,8 +10,8 @@ def asset_path(app_name):
         apps = []
     ass_path = ""
 
-    if f"github_user_tildagon_{app_name}" in apps:
-        ass_path = f"/apps/github_user_tildagon_{app_name}/"
+    if f"pikesley_tildagon_{app_name}" in apps:
+        ass_path = f"/apps/pikesley_tildagon_{app_name}/"
 
     if app_name in apps:
         ass_path = f"apps/{app_name}/"
